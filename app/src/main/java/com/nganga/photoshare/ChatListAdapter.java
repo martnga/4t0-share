8d9433d79dd3351da8ab5c19d566d32ca9551437
mtnjoroge <mtnjoroge@gmail.com>
2015-08-03 15:14:29 +0300

    changed ref type to com.firebase.client.Query;

--- app/src/main/java/com/nganga/photoshare/ChatListAdapter.java
+++ app/src/main/java/com/nganga/photoshare/ChatListAdapter.java
@@ -1,10 +1,11 @@
 package com.nganga.photoshare;
 
 import android.app.Activity;
-import android.app.DownloadManager;
 import android.graphics.Color;
 import android.view.View;
 import android.widget.TextView;
+
+import com.firebase.client.Query;
 
 /**
  * Created by nganga on 8/3/15.
@@ -13,7 +14,7 @@ public class ChatListAdapter  extends FirebaseListAdapter<Chat> {
 
     private String mUsername;
 
-    public ChatListAdapter(DownloadManager.Query ref, Activity activity, int layout, String mUsername) {
+    public ChatListAdapter(Query ref, Activity activity, int layout, String mUsername) {
         super(ref, Chat.class, layout, activity);
         this.mUsername = mUsername;
 
