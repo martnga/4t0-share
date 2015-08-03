f5b929245513542974f5b418d0190588022dd9ec
mtnjoroge <mtnjoroge@gmail.com>
2015-08-03 15:18:09 +0300

    declared android context to allow communication

--- app/src/main/java/com/nganga/photoshare/ChatApplication.java
+++ app/src/main/java/com/nganga/photoshare/ChatApplication.java
@@ -1,7 +1,14 @@
 package com.nganga.photoshare;
 
+import com.firebase.client.Firebase;
+
 /**
  * Created by nganga on 8/3/15.
  */
-public class ChatApplication {
+public class ChatApplication extends android.app.Application {
+      @Override
+       public void onCreate() {
+        super.onCreate();
+        Firebase.setAndroidContext(this);
+        }
 }
