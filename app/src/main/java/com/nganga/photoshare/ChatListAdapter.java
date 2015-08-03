43096b64a68b67a58eac7bbccbef02cbc7f15d56
mtnjoroge <mtnjoroge@gmail.com>
2015-08-03 14:24:31 +0300

    created format for chat messages in the list

--- app/src/main/java/com/nganga/photoshare/ChatListAdapter.java
+++ app/src/main/java/com/nganga/photoshare/ChatListAdapter.java
@@ -1,7 +1,18 @@
 package com.nganga.photoshare;
 
+import android.app.Activity;
+import android.app.DownloadManager;
+
 /**
  * Created by nganga on 8/3/15.
  */
 public class ChatListAdapter  extends FirebaseListAdapter<Chat> {
+
+    private String mUsername;
+
+    public ChatListAdapter(DownloadManager.Query ref, Activity activity, int layout, String mUsername) {
+        super(ref, Chat.class, layout, activity);
+        this.mUsername = mUsername;
+
+    }
 }
